a4d181d9e440c1ba7cc9be99036825d6ef3a4298
ThangTheMan <thang@fromttm.com>
2015-05-27 16:33:03 -0700

    chat + logic

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",
     //Chat box
     function($scope, $firebaseArray) {
         // CREATE A REFERENCE TO FIREBASE
-
+        var message1, newPost ;
+        $scope.messageResult = "test";
         // CREATE A REFERENCE TO FIREBASE
         var messagesRef = new Firebase('https://task4thang.firebaseio.com/Chat');
-
+        var username, textResult;
+        $scope.messageResult = textResult;
+        var pixelDataRef = new Firebase('https://task4thang.firebaseio.com/Drawing');
         // REGISTER DOM ELEMENTS
         var messageField = $('#messageInput');
         var nameField = $('#nameInput');
@@ -19,20 +22,42 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",
         messageField.keypress(function (e) {
             if (e.keyCode == 13) {
                 //FIELD VALUES
-                var username = nameField.val();
+                username = nameField.val();
                 var message = messageField.val();
-
+                message1 = message;
                 //SAVE DATA TO FIREBASE AND EMPTY FIELD
                 messagesRef.push({name:username, text:message});
                 messageField.val('');
             }
+
+            if (message1 == newPost.value ) {
+                var result = new Firebase("https://task4thang.firebaseio.com/Result");
+
+
+                    result.push({value: username + " won the game"});
+
+
+
+                   console.log($scope.messageResult);
+
+                    console.log("Testing: " + result.value);
+
+
+
+
+
+                game.remove();
+                pixelDataRef.remove();
+                result.remove();
+            };
         });
+
 
         // Add a callback that is triggered for each chat message.
         messagesRef.limitToLast(10).on('child_added', function (snapshot) {
             //GET DATA
             var data = snapshot.val();
-            var username = data.name || "anonymous";
+            username = data.name || "anonymous";
             var message = data.text;
 
             //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
@@ -46,31 +71,41 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",
 
             //SCROLL TO BOTTOM OF MESSAGE LIST
             messageList[0].scrollTop = messageList[0].scrollHeight;
-        });
-            var game = new Firebase("https://task4thang.firebaseio.com/Game");
 
-          ////ADD MESSAGE METHOD
-          //  $scope.addAnswer = function (a) {
-          //      //LISTEN FOR RETURN KEY
-          //      if (a.keyCode === 13 && $scope.ans) {
-          //          //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-          //           var answer = $scope.ans;
-          //
-          //          //ADD TO FIREBASE
-          //
-          //          game.child('Answer').push({value: answer});
-          //
-          //          answer1= answer;
-          //
-          //          //RESET MESSAGE
-          //          $scope.ans = "";
-          //
-          //
-          //
-          //      }
-          //  };
+        });
+
+
+        var game = new Firebase("https://task4thang.firebaseio.com/Game");
+            game.once("child_added", function(snapshot) {
+                newPost = snapshot.val();
+            console.log("Answer: " + newPost.value);
+        });
+
+          //ADD MESSAGE METHOD
+            $scope.addAnswer = function (a) {
+                //LISTEN FOR RETURN KEY
+                if (a.keyCode === 13 && $scope.ans) {
+                    //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+                    var answer = $scope.ans;
+
+                    //ADD TO FIREBASE
+
+                    game.push({value: answer});
+
+
+
+
+                    //RESET MESSAGE
+                    $scope.ans = "";
+
+
+
+                }
+            };
 
 
     }
+
+
 ]);
 
